df6e69781dd13851e57e4945894834630cf053ef
georginafp <georgina.fuentes@upf.edu>
2025-06-27 11:18:47 +0200

    add full pipeline steps + change README

--- R/run_spicey.R
+++ R/run_spicey.R
@@ -1,80 +1,132 @@
 #' Run full SPICEY pipeline: compute RETSI and GETSI, annotate regulatory elements, and link measures
 #'
 #' This function performs the complete SPICEY pipeline workflow:
-#' 1. Computes Regulatory Element Tissue Specificity Index (RETSI) from scATAC-seq DE results.
-#' 2. Computes Gene Expression Tissue Specificity Index (GETSI) from scRNA-seq DE results.
+#' 1. Computes Regulatory Element Tissue Specificity Index (RETSI) from scATAC-seq differential accessibility results.
+#' 2. Computes Gene Expression Tissue Specificity Index (GETSI) from scRNA-seq differential expression results.
 #' 3. Annotates regulatory elements (RE) with nearest genes.
-#' 4. Optionally annotates RE with co-accessible target genes if `link_method = "coaccessible"`.
+#' 4. Optionally annotates RE with co-accessible target genes if `link_method = "coaccessibility"`.
 #' 5. Links RETSI and GETSI scores either by nearest gene or co-accessibility links.
 #'
-#' @param atac_data A data.frame or tibble containing differential accessibility results used to compute RETSI.
-#' @param rna_data A data.frame or tibble containing differential expression results used to compute GETSI.
-#' @param re_gr A GRanges object representing regulatory elements to be annotated and linked.
-#' @param gene_gr A GRanges object representing genes used for nearest or co-accessibility annotation.
-#' @param link_method Character string specifying linking method. One of `"nearest"` (default) or `"coaccessible"`.
-#' @param coaccess_links Optional. A data.frame or GRangesList representing co-accessibility links. Required if `link_method = "coaccessible"`.
+#' @param atac A \code{GRanges} or \code{data.frame} convertible to \code{GRanges} containing differential accessibility results. Must include \code{seqnames}, \code{start}, \code{end}, \code{avg_log2FC}, \code{p_val}, and \code{cell_type} columns.
+#' @param rna A \code{GRanges} or \code{data.frame} convertible to \code{GRanges} containing differential expression results. Must include \code{seqnames}, \code{start}, \code{end}, \code{avg_log2FC}, \code{p_val}, and \code{cell_type} columns.
+#' @param link_method Character string specifying the linking method. One of \code{"nearest"} or \code{"coaccessibility"}. If \code{NULL} (default), linking is not performed.
+#' @param links Optional. A \code{data.frame} or \code{GRangesList} of co-accessibility links. Required if \code{link_method = "coaccessibility"}.
+#' @param coaccess_cutoff_override Numeric scalar. Co-accessibility score cutoff used to filter links. Default is \code{0.25}.
+#' @param filter_promoter_distal Logical. Whether to filter links to keep only promoter-distal interactions when using co-accessibility. Default is \code{TRUE}.
 #'
 #' @return A list containing:
 #' \describe{
-#'   \item{retsi}{Data frame with computed RETSI scores.}
-#'   \item{getsi}{Data frame with computed GETSI scores.}
-#'   \item{re_gr_annotated}{GRanges object with regulatory elements annotated with nearest and/or co-accessible genes.}
-#'   \item{linked}{Data frame linking RETSI and GETSI scores according to chosen linking method.}
+#'   \item{retsi}{Data frame with computed RETSI scores if \code{atac} provided.}
+#'   \item{getsi}{Data frame with computed GETSI scores if \code{rna} provided.}
+#'   \item{linking}{Data frame linking RETSI and GETSI scores if \code{link_method} specified.}
 #' }
 #'
 #' @examples
 #' \dontrun{
-#' # Run with nearest gene linking (default)
-#' results <- run_spicey_full_pipeline(atac_data, rna_data, re_gr, gene_gr)
+#' # Compute GETSI only
+#' results <- run_spicey_pipeline(rna = rna_data)
 #'
-#' # Run with co-accessibility linking
-#' results <- run_spicey_full_pipeline(
-#'   atac_data, rna_data, re_gr, gene_gr,
-#'   link_method = "coaccessible",
-#'   coaccess_links = coaccessibility_links
+#' # Compute RETSI only
+#' results <- run_spicey_pipeline(atac = atac_data)
+#'
+#' # Compute both GETSI and RETSI without linking
+#' results <- run_spicey_pipeline(atac = atac_data, rna = rna_data)
+#'
+#' # Compute both and link with nearest gene method
+#' results <- run_spicey_pipeline(atac = atac_data, rna = rna_data, link_method = "nearest")
+#'
+#' # Compute both and link with co-accessibility method
+#' results <- run_spicey_pipeline(
+#'   atac = atac_data,
+#'   rna = rna_data,
+#'   link_method = "coaccessibility",
+#'   links = coaccessibility_links,
+#'   coaccess_cutoff_override = 0.3,
+#'   filter_promoter_distal = TRUE
 #' )
 #' }
 #'
 #' @export
-run_spicey <- function(
-    atac_data,
-    rna_data,
-    re_gr,
-    gene_gr,
-    link_method = c("nearest", "coaccessible"),
-    coaccess_links = NULL
-) {
-  link_method <- match.arg(link_method)
+run_spicey <- function(atac = NULL,
+                                rna = NULL,
+                                link_method = NULL,
+                                links = NULL,
+                                coaccess_cutoff_override = 0.25,
+                                filter_promoter_distal = TRUE) {
+  # Load TxDb for annotation (adjust as needed)
+  txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene::TxDb.Hsapiens.UCSC.hg38.knownGene
 
-  # 1. Compute RETSI
-  retsi_df <- spicey_retsi(atac_data)
+  # Check inputs
+  if (is.null(atac) && is.null(rna)) {
+    stop("At least one of 'atac' or 'rna' data must be provided.")
+  }
 
-  # 2. Compute GETSI
-  getsi_df <- spicey_getsi(rna_data)
+  # Initialize output list
+  results <- list()
 
-  # 3. Annotate regulatory elements with nearest gene
-  re_gr <- annotate_with_nearest(re_gr, gene_gr)
+  # Compute RETSI if atac provided
+  if (!is.null(atac)) {
+    message("Computing RETSI...")
+    retsi <- spicey_retsi(atac)
+    results$retsi <- retsi
+  }
 
-  # 4. Annotate with co-accessible target genes if requested
-  if (link_method == "coaccessible") {
-    if (is.null(coaccess_links)) {
-      stop("coaccess_links must be provided if link_method = 'coaccessible'")
+  # Compute GETSI if rna provided
+  if (!is.null(rna)) {
+    message("Computing GETSI...")
+    getsi <- spicey_getsi(rna)
+    results$getsi <- getsi
+  }
+
+  # If both atac and rna provided, and no link_method, just return both indices
+  if (!is.null(atac) && !is.null(rna) && is.null(link_method)) {
+    return(results)
+  }
+
+  # If linking requested, check required inputs and perform linking
+  if (!is.null(link_method)) {
+    if (is.null(atac) || is.null(rna)) {
+      stop("Both 'atac' and 'rna' data must be provided for linking.")
     }
-    re_gr <- annotate_with_coaccessibility(re_gr, gene_gr, coaccess_links)
+    if (!(link_method %in% c("nearest", "coaccessibility"))) {
+      stop("link_method must be either 'nearest' or 'coaccessibility'")
+    }
+
+    if (link_method == "nearest") {
+      message("Linking regulatory elements to genes using nearest gene method...")
+      # Annotate RE with nearest genes
+      retsi_gene_nearest <- annotate_with_nearest(results$retsi)
+      # Link spicey using nearest method
+      spicey_nearest <- link_spicey_nearest(retsi_gene_nearest, results$getsi)
+      results$linking <- spicey_nearest
+    }
+
+    if (link_method == "coaccessibility") {
+      if (is.null(links)) {
+        stop("Argument 'links' must be provided when link_method = 'coaccessibility'")
+      }
+      message("Filtering and annotating coaccessibility links...")
+
+      # Annotate links with CCANs and filter promoter-distal if requested
+      annotated_links <- annotate_links_with_ccans(
+        links = links,
+        coaccess_cutoff_override = coaccess_cutoff_override,
+        filter_promoter_distal = filter_promoter_distal,
+        txdb = txdb
+      )
+
+      message("Linking regulatory elements to genes using coaccessibility method...")
+      spicey_coacc <- link_spicey_coaccessible(
+        re = results$retsi,
+        getsi = results$getsi,
+        links = annotated_links,
+        txdb = txdb,
+        name_links = "coacc"
+      )
+      results$linking <- spicey_coacc
+    }
   }
 
-  # 5 & 6. Link spicey measures based on chosen method
-  if (link_method == "nearest") {
-    linked <- link_spicey_nearest(retsi_df, getsi_df, re_gr)
-  } else {
-    linked <- link_spicey_coaccessibility(retsi_df, getsi_df, re_gr)
-  }
-
-  # Return results list
-  list(
-    retsi = retsi_df,
-    getsi = getsi_df,
-    re_gr_annotated = re_gr,
-    linked = linked
-  )
+  return(results)
 }
+
--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# SPICEY <img src="man/figures/logo_spicey.png" width="140px" height="140px" align="right" style="padding-left:10px;background-color:white;" />
+# SPICEY <img src="man/figures/logo_spicey.png" width="140px" height="140px" align="right" style="padding-left:10px;background-color:white;"/>
 
 <!-- badges: start -->
+
 <!-- badges: end -->
 
 ## Bioconductor release status
 
-|      Branch      |    R CMD check   | Last updated |
-|:----------------:|:----------------:|:------------:|
-| [_devel_](http://bioconductor.org/packages/devel/bioc/html/SPICEY.html) | [![Bioconductor-devel Build Status](http://bioconductor.org/shields/build/devel/bioc/SPICEY.svg)](http://bioconductor.org/checkResults/devel/bioc-LATEST/SPICEY) | ![](http://bioconductor.org/shields/lastcommit/devel/bioc/SPICEY.svg) |
-| [_release_](http://bioconductor.org/packages/release/bioc/html/SPICEY.html) | [![Bioconductor-release Build Status](http://bioconductor.org/shields/build/release/bioc/SPICEY.svg)](http://bioconductor.org/checkResults/release/bioc-LATEST/SPICEY) | ![](http://bioconductor.org/shields/lastcommit/release/bioc/SPICEY.svg) |
+| Branch | R CMD check | Last updated |
+|:------------------------:|:------------------------:|:-------------------:|
+| [*devel*](http://bioconductor.org/packages/devel/bioc/html/SPICEY.html) | [![Bioconductor-devel Build Status](http://bioconductor.org/shields/build/devel/bioc/SPICEY.svg)](http://bioconductor.org/checkResults/devel/bioc-LATEST/SPICEY) | ![](http://bioconductor.org/shields/lastcommit/devel/bioc/SPICEY.svg) |
+| [*release*](http://bioconductor.org/packages/release/bioc/html/SPICEY.html) | [![Bioconductor-release Build Status](http://bioconductor.org/shields/build/release/bioc/SPICEY.svg)](http://bioconductor.org/checkResults/release/bioc-LATEST/SPICEY) | ![](http://bioconductor.org/shields/lastcommit/release/bioc/SPICEY.svg) |
 
 The goal of SPICEY is to provide a user-friendly pipeline for quantifying and visualizing tissue specificity specificity
 
@@ -16,7 +17,9 @@ The goal of SPICEY is to provide a user-friendly pipeline for quantifying and vi
 
 You can install the latest release of `SPICEY` the github repository:
 
-    devtools::install_github("georginafp/SPICEY")
+```         
+devtools::install_github("georginafp/SPICEY")
+```
 
 Now you can load the package using `library(SPICEY)`.
 
@@ -29,26 +32,29 @@ library(SPICEY)
 ```
 
 ``` r
-## Nearest gene mode
-result_nearest <- run_spicey(
-  atac_path = system.file("extdata", "FINAL_ATAC.rds", package = "SPICEY"),
-  rna_path = system.file("extdata", "FINAL_RNA.rds", package = "SPICEY"),
-  linking_method = "nearest"
-)
+# Compute GETSI 
+results <- run_spicey(rna = rna)
 
-## Co-accessibility mode
-result_coacc <- run_spicey(
-  atac_path = system.file("extdata", "FINAL_ATAC.rds", package = "SPICEY"),
-  rna_path = system.file("extdata", "FINAL_RNA.rds", package = "SPICEY"),
-  links_path = system.file("extdata", "COACC_LINKS.rds", package = "SPICEY"),
-  linking_method = "coaccessibility"
-)
+# Compute RETSI
+results <- run_spicey(atac=atac)
 
+# Compute GETSI + RETSI
+results <- run_spicey(atac=atac, rna=rna)
+
+# Compute GETSI + RETSI and link RE to target genes through nearest gene method
+results <- run_spicey(rna = rna, atac=atac, link_method = "nearest")
+
+# Compute GETSI + RETSI and link RE to target genes through coaccessibility method
+results <- run_spicey(
+  atac = atac_data,
+  rna = rna_data,
+  link_method = "coaccessibility",
+  links = coaccessibility_links,
+  coaccess_cutoff_override = 0.25,  # default
+  filter_promoter_distal = TRUE     # default
+)
 ```
 
 ## Code of Conduct
 
-Please note that the SPICEY project is released with a [Contributor
-Code of
-Conduct](https://contributor-covenant.org/version/2/0/CODE_OF_CONDUCT.html).
-By contributing to this project, you agree to abide by its terms.
+Please note that the SPICEY project is released with a [Contributor Code of Conduct](https://contributor-covenant.org/version/2/0/CODE_OF_CONDUCT.html). By contributing to this project, you agree to abide by its terms.
